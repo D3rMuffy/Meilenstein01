f99629f5c77480eef49f903702ceb906ecc6d227
Chris_Desktop <39556823+D3rMuffy@users.noreply.github.com>
2018-05-25 21:52:50 +0200

    finished methods getRowValuePerm and isRowValuePermutation in class RowUtils; added Class GridUtil3

---
+++ 03 Omfg/src/utils/GridUtils3.java
@@ -0,0 +1,22 @@
+package utils;
+
+import data.Grid;
+
+public class GridUtils3 implements GridUtil3{
+
+	public void turnRight(Grid grid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isGridTurn(Grid grid1, Grid grid2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int getGridTurnNumber(Grid grid1, Grid grid2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
